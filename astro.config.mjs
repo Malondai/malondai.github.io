6e7e237e20b48a0b8bee51dfcb6e6f8744227239
Malondai <atir417@gmail.com>
2026-05-05 23:00:32 +0800

    update portofolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://malondai.github.io', 
+    base: '/portofolio', // Sesuaikan jika ini adalah sub-folder (repository name)
     vite: {
         plugins: [tailwindcss()],
     },
