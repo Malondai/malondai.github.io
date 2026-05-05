09292e7fbd0f2e5872a93e8d7acc44baba332eb5
Malondai <atir417@gmail.com>
2026-05-05 23:07:31 +0800

    adjusment url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
     site: 'https://malondai.github.io', 
-    base: '/portofolio', // Sesuaikan jika ini adalah sub-folder (repository name)
+    base: '/', 
     vite: {
         plugins: [tailwindcss()],
     },
